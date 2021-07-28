7c23550b3fbf40342eafe4ca5eaaf454abada44e
Deepak Rattan <mca.deepakrattan@gmail.com>
2021-07-28 19:33:11 +0530

    Design of Login Screen implemented

--- app/src/main/java/com/android/mvvmlogindemo/ui/signin/LoginActivity.kt
+++ app/src/main/java/com/android/mvvmlogindemo/ui/signin/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.android.mvvmlogindemo
+package com.android.mvvmlogindemo.ui.signin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.mvvmlogindemo.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
