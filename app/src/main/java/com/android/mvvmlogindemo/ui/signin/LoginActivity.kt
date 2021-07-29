eccbaed8cbc7a8d527600e32abcbc1feb393dfc6
Deepak Rattan <mca.deepakrattan@gmail.com>
2021-07-29 22:25:58 +0530

    LoginActivity Implemented

--- app/src/main/java/com/android/mvvmlogindemo/ui/signin/LoginActivity.kt
+++ app/src/main/java/com/android/mvvmlogindemo/ui/signin/LoginActivity.kt
@@ -1,12 +1,33 @@
 package com.android.mvvmlogindemo.ui.signin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.android.mvvmlogindemo.R
+import com.android.mvvmlogindemo.databinding.ActivityLoginBinding
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+
+    // Inject ViewModel
+    private val loginViewModel: LoginViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.loginViewModel = loginViewModel
+        binding.lifecycleOwner = this
+
+        loginViewModel.loginResponse.observe(this, Observer {
+            Log.d("test", "success : ${it.fullname}")
+        })
+
+        loginViewModel.isError.observe(this, Observer {
+            Log.d("test", "error :$it ")
+        })
     }
 }
