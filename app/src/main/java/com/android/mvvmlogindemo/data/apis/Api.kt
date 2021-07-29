787270a6c3ee0e53e3cdb5d9474b64bedab64b87
Deepak Rattan <mca.deepakrattan@gmail.com>
2021-07-29 22:11:33 +0530

    login function added to interface Api

--- app/src/main/java/com/android/mvvmlogindemo/data/apis/Api.kt
+++ app/src/main/java/com/android/mvvmlogindemo/data/apis/Api.kt
@@ -1,7 +1,7 @@
 package com.android.mvvmlogindemo.data.apis
 
 import com.android.mvvmlogindemo.data.models.LoginResponse
-import com.android.mvvmlogindemo.utils.constants.Constant
+import com.android.mvvmlogindemo.utils.Constant
 import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -11,7 +11,7 @@ interface Api {
     @FormUrlEncoded
     @POST(Constant.LOGIN)
     suspend fun login(
-        @Field("username") username: String,
-        @Field("password") password: String
+        @Field("username") username: String?,
+        @Field("password") password: String?
     ) : Response<LoginResponse>
 }
